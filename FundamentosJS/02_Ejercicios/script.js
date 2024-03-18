a3a6e233641a31eab96a3684304f326364791d02
gota-csm1~ <matiasbustamante@liceovvh.cl>
2024-03-18 11:11:04 -0300

    ok

---
+++ FundamentosJS/02_Ejercicios/script.js
@@ -0,0 +1,33 @@
+console.log("Vinculado correctamente...")
+// Ejercicio 1: Declaración de variables
+// Instrucciones: Declara dos variables numéricas llamadas 'ahora y 'fecha_nac'.
+// Luego, calcula y muestra en un mensaje la edad actual basada en el año de nacimiento.
+
+function declararVariables() {
+    alert("Esto es una alerta")
+    // Tu código aquí
+    }
+    
+    // Ejercicio 2: Asignación de valores
+    // Instrucciones: Completa la función para asignar el valor de 'x' a 'y' y luego muestra el valor de 'y'.
+    function asignarValores() {
+    
+    // Tu código aquí
+    }
+    
+    // Ejercicio 3: Operaciones matemáticas
+    // Instrucciones: Completa la función para realizar las siguientes operaciones:
+    // Suma 'num1' y 'num2' y 'num3', resta 'num1' de 'num3', multiplica 'num2' por 'num3', y divide 'num1' entre '120'.
+    // Muestra los resultados en un mensaje.
+    function realizarOperaciones() {
+    alert("")
+    // Tu código aquí
+    }
+    
+    // Ejercicio 4: Manipulación de cadenas
+    // Instrucciones: Completa la función para construir un mensaje utilizando las variables 'direccion' y 'num_casa'.
+    // Luego, muestra la longitud del mensaje y el primer carácter en el mensaje.
+    function manipularCadenas() {
+    
+    // Tu código aquí
+    }
