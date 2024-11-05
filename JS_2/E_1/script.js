e6eddf9bf0a66f5a9be44dddda60d0889b608435
Matías Bustamante <139813570+gota-csm@users.noreply.github.com>
2024-11-04 21:15:24 -0300

    ok

--- JS_2/E_1/script.js
+++
@@ -1,5 +0,0 @@
-function ejemplo(elemento) {
-    console.log("elemento clickeado", elemento);
-    elemento.style.backgroundColor = 'blue'; // Cambia el color de fondo del botón
-    elemento.textContent = 'El Primo is here 🗣️🔥'; // Cambia el texto del botón
-}
